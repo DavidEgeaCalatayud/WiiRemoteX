21b1300876666a16cd66b80fd7e1631295bbb124
David Egea Calatayud <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-23 00:54:35 +0200

    Convert Wii core modules to real Kotlin Multiplatform source sets

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -13,27 +13,14 @@ kotlin {
     jvmToolchain(17)
 
     sourceSets {
-        commonMain {
-            kotlin.srcDirs(
-                "../core/model/src/main/kotlin",
-                "../core/protocol/src/main/kotlin",
-                "../core/session/src/main/kotlin",
-                "src/commonMain/kotlin",
-            )
-            dependencies {
-                implementation("org.jetbrains.kotlinx:atomicfu:0.33.0")
-            }
+        commonMain.dependencies {
+            implementation(project(":core:model"))
+            implementation(project(":core:protocol"))
+            implementation(project(":core:session"))
         }
 
-        commonTest {
-            kotlin.srcDirs(
-                "../core/protocol/src/test/kotlin",
-                "../core/session/src/test/kotlin",
-                "src/commonTest/kotlin",
-            )
-            dependencies {
-                implementation(kotlin("test"))
-            }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
         }
     }
 
