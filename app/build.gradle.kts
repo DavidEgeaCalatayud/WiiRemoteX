b1000dbb748e86d3b40dcc0851b89cd94524819a
David Egea Calatayud <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-22 15:36:18 +0200

    build: use stable Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "io.github.davidegeacalatayud.wiiremotex"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.davidegeacalatayud.wiiremotex"
