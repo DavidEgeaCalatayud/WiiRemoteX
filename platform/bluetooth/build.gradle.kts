4686a54bdc109cd6a07c83790587bb667f58c8ae
David Egea Calatayud <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-22 15:36:21 +0200

    build: use stable Android API 36

--- platform/bluetooth/build.gradle.kts
+++ platform/bluetooth/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.library") }
 android {
     namespace = "io.github.davidegeacalatayud.wiiremotex.platform.bluetooth"
-    compileSdk = 37
+    compileSdk = 36
     defaultConfig { minSdk = 28 }
 }
 dependencies {
