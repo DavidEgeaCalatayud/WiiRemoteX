61989db5539de78827852fecbf982ef2d23950b5
David Egea Calatayud <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-22 15:54:50 +0200

    chore: bump PoC version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.davidegeacalatayud.wiiremotex"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-poc"
+        versionCode = 2
+        versionName = "0.1.1-poc"
     }
 
     buildFeatures {
