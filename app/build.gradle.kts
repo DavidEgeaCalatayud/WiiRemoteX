201f261afa623509f5db7bddaf42fefb69a636f5
David Egea Calatayud <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-22 16:57:25 +0200

    chore: bump advanced emulator build to 0.2.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.davidegeacalatayud.wiiremotex"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1-poc"
+        versionCode = 3
+        versionName = "0.2.0-alpha"
     }
 
     buildFeatures {
