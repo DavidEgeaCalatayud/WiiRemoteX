b319d06b74887781c3294a88ff12ee13cf6f1f05
David Egea Calatayud <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-23 17:03:33 +0200

    Add Kotlin static analysis and style plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,11 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "2.4.20" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.4.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
 }
